2533ae23f6fc244a8b5e696b71d85e255e9587fd
vspiridonoff <you@example.com>
2020-04-15 14:27:45 +0300

    Add bootstrap nav, products

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
 let countDownDate = new Date("May 1, 2020 12:00:00").getTime();
 
 // Update the count down every 1 second
-const x = setInterval(function() {
+const x = setInterval(function () {
 
   // Get today's date and time
   const now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   const distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   // Output the result in an element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
+    + minutes + "m " + seconds + "s ";
+
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
